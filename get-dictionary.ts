d658bf37e57b04263e16232838b4e2567ef63b5c
Ahmet Tuna POTUR <tpotur@gmail.com>
2026-04-20 19:09:59 +0300

    nextjs Minimal i18n routing and translations

--- get-dictionary.ts
+++ get-dictionary.ts
@@ -7,6 +7,7 @@ const dictionaries = {
   en: () => import("./dictionaries/en.json").then((module) => module.default),
   de: () => import("./dictionaries/de.json").then((module) => module.default),
   cs: () => import("./dictionaries/cs.json").then((module) => module.default),
+  tr: () => import("./dictionaries/tr.json").then((module) => module.default),
 };
 
 export const getDictionary = async (locale: Locale) =>
